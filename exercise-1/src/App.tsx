5d0937ed8a161afd2ceec698e55306c78341d6df
Gosei Vietnam <pham.duy.tung@gosei.com.vn>
2023-06-07 15:31:22 +0700

    ex1

--- exercise-1/src/App.tsx
+++ exercise-1/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
 
-export default function App() {
-  return (
-    <div>Hello World!</div>
-  )
-}
+const HelloWorld: React.FC = () => {
+  console.log("Hello World");
+  return null;
+};
+
+export default HelloWorld;
